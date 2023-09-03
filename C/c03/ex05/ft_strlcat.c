6b73980f5e4fc4c63dceb2fc2512f9bfec75b4f7
dgomes-a <dgomes-a@student.42porto.com>
2023-09-03 16:02:32 +0100

    c03 ex5

--- C/c03/ex05/ft_strlcat.c
+++ C/c03/ex05/ft_strlcat.c
@@ -6,8 +6,53 @@
 /*   By: dgomes-a <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/02 17:48:40 by dgomes-a          #+#    #+#             */
-/*   Updated: 2023/09/02 17:48:41 by dgomes-a         ###   ########.fr       */
+/*   Updated: 2023/09/03 16:01:09 by dgomes-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int counter;
 
+	counter = 0;
+	while (*str != '\0')
+	{
+		counter++;
+		str++;
+	}
+	return (counter);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int c;
+	unsigned int d;
+
+	if (size <= ft_strlen(dest))
+		return (size + ft_strlen(src));
+	c = ft_strlen(dest);
+	d = 0;
+	while (src[d] != '\0' && c + 1 < size)
+	{
+		dest[c] = src[d];
+		c++;
+		d++;
+	}
+	dest[c] = '\0';
+	return (ft_strlen(dest) + ft_strlen(&src[d]));
+}
+
+#include <stdio.h>
+int		main(void)
+{
+	char dest[20] = "123";
+	char src[] = "4567890";
+	unsigned int size = 6;
+	unsigned int result;
+
+	printf("-----\ndest = %s\nsrc = %s\nnb = %d\n\n", dest, src, size);
+	result = ft_strlcat(dest, src, size);
+	printf("dest (cat) = %s\nresult = %d\n-----\n", dest, result);
+
+	return (0);
+}
