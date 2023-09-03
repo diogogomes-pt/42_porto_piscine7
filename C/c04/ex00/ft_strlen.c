b4eb701632e12c25468cd390b65c2d847c7eea7b
Diogo Gomes <100311063+diogogomes-pt@users.noreply.github.com>
2023-09-03 22:53:28 +0100

    Add files via upload

---
+++ C/c04/ex00/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgomes-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 19:32:36 by dgomes-a          #+#    #+#             */
+/*   Updated: 2023/09/03 20:42:08 by dgomes-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	printf("%d\n", ft_strlen("Porto 42 len "));
+	printf("%lu", strlen("Porto 42 len "));
+}
